cc6d3f12c1a082019e5e90e64766d1805bf713ec
Mostafa Taheri <smt4940@gmail.com>
2019-02-20 03:31:03 -0800

    0.01

---
+++ src/test/java/co/nilin/spring/cloud/consulall/ConsulAllApplicationTests.java
@@ -0,0 +1,16 @@
+package co.nilin.spring.cloud.consulall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsulAllApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
