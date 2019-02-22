d14e5ff5156c9fb38de565a895e6503f775d1d1f
Mostafa Tahery <smt4940@gmail.com>
2019-02-22 09:52:15 +0330

    0.01

--- src/main/java/co/nilin/spring/cloud/consulall/ConsulAllApplication.java
+++ src/main/java/co/nilin/spring/cloud/consulall/ConsulAllApplication.java
@@ -1,12 +1,26 @@
 package co.nilin.spring.cloud.consulall;
 
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@RestController
 @SpringBootApplication
 public class ConsulAllApplication {
+
+	@RequestMapping(method = RequestMethod.GET,value = "/")
+	public String show(){
+		return "Hello";
+	}
+
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ConsulAllApplication.class).web(WebApplicationType.NONE).run(args);
