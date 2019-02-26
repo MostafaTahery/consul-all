9e1ae7571b58f6766f1d8b55ae64100a0fc3f0f7
Mostafa Tahery <smt4940@gmail.com>
2019-02-26 13:41:44 +0330

    0.02 rest Template - feign Client

--- src/main/java/co/nilin/spring/cloud/consulall/Controller/DistributedPropertiesController.java
+++ src/main/java/co/nilin/spring/cloud/consulall/Controller/DistributedPropertiesController.java
@@ -5,12 +5,13 @@ import co.nilin.spring.cloud.consulall.utils.MyProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@EnableFeignClients
 public class DistributedPropertiesController {
 
     @Value("${my.prop}")
@@ -24,7 +25,7 @@ public class DistributedPropertiesController {
         return value;
     }
 
-    @GetMapping("getConfigFromProperty")
+    @GetMapping("/getConfigFromProperty")
     public String getConfigFromProperty(){
         return properties.getProp();
     }
